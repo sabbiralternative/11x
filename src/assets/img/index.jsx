b8e339e7b6bbadf5e386172812381e1130de2d20
sabbirHossain <sabbirshnt@gmail.com>
2025-03-19 11:38:43 +0600

    done

--- src/assets/img/index.jsx
+++ src/assets/img/index.jsx
@@ -5,8 +5,10 @@ import bankPicture from "../icon/bankPicture.svg";
 import deleteIcon from "../icon/delete-icon.svg";
 import edit from "../icon/edit.svg";
 import tv from "../images/tv.svg";
+import logo from "../images/logo.png";
 
 export default {
+  logo,
   tv,
   edit,
   deleteIcon,
