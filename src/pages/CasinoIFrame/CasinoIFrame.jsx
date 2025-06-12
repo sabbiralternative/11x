6d14d5e37cb1f87ed2b1326cd18bfda52b9c9a0f
sabbirHossain <sabbirshnt@gmail.com>
2025-06-12 16:55:55 +0600

    done

--- src/pages/CasinoIFrame/CasinoIFrame.jsx
+++ src/pages/CasinoIFrame/CasinoIFrame.jsx
@@ -3,11 +3,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useLiveCasinoIframeMutation } from "../../redux/features/casino/casino.api";
 import { useSelector } from "react-redux";
 import { Settings } from "../../api";
+import Loader from "../../components/shared/Loader/Loader";
 
 const CasinoIFrame = () => {
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-  const [handleGetIFrame, { data }] = useLiveCasinoIframeMutation();
+  const [handleGetIFrame, { data, isLoading, isSuccess }] =
+    useLiveCasinoIframeMutation();
   const { gameId } = useParams();
 
   useEffect(() => {
@@ -20,6 +22,7 @@ const CasinoIFrame = () => {
 
     handleGetIFrame(payload);
   }, [handleGetIFrame, gameId]);
+
   return (
     <div>
       <div className="ng-star-inserted">
@@ -30,6 +33,7 @@ const CasinoIFrame = () => {
             </button>
             <span>{user}</span>
           </h2>
+          {isLoading && !isSuccess && <Loader />}
           <iframe
             style={{ minHeight: "100vh", width: "100%" }}
             allowfullscreen="true"
