b32b60849e2cfc791c141f3518a48ff9db80a74d
sabbirHossain <sabbirshnt@gmail.com>
2025-08-04 17:12:08 +0600

    done

--- src/pages/Deposit/PaymentMethods.jsx
+++ src/pages/Deposit/PaymentMethods.jsx
@@ -167,7 +167,13 @@ const PaymentMethods = ({
                     {method?.type == "usdt" ? (
                       <img
                         style={{ height: "20px", width: "20px" }}
-                        src={img.usdt}
+                        src={"/icon/trc20.svg"}
+                      />
+                    ) : null}
+                    {method?.type == "usdt_bep20" ? (
+                      <img
+                        style={{ height: "20px", width: "20px" }}
+                        src={"/icon/bep20.svg"}
                       />
                     ) : null}
                     {method?.type == "whatsapp" ? (
@@ -700,7 +706,7 @@ const PaymentMethods = ({
           </div>
         </div>
       )}
-      {tabs === "usdt" && (
+      {tabs === "usdt" || tabs === "usdt_bep20" ? (
         <div _ngcontent-kdb-c159="" className="paymethod ng-tns-c159-13">
           <div _ngcontent-kdb-c159="" className="accountdetail ng-tns-c159-13">
             <div _ngcontent-kdb-c159="" className="paymethod ng-tns-c159-13">
@@ -890,7 +896,7 @@ const PaymentMethods = ({
             </div>
           </div>
         </div>
-      )}
+      ) : null}
       {tabs === "pg" && qrcode && isDesktop && (
         <div _ngcontent-kdb-c159="" className="paymethod ng-tns-c159-13">
           <div _ngcontent-kdb-c159="" className="accountdetail ng-tns-c159-13">
