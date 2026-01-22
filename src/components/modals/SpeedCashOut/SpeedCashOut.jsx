f7d0298952c3fe85f3383c4ce0e2e05f208f8b08
sabbirHossain <sabbirshnt@gmail.com>
2026-01-22 14:44:47 +0600

    done

--- src/components/modals/SpeedCashOut/SpeedCashOut.jsx
+++ src/components/modals/SpeedCashOut/SpeedCashOut.jsx
@@ -10,6 +10,7 @@ import useCloseModalClickOutside from "../../../hooks/closeModal";
 import { useIndex } from "../../../hooks";
 
 const SpeedCashOut = ({ speedCashOut, setSpeedCashOut }) => {
+  const closePopupForForever = localStorage.getItem("closePopupForForever");
   const { eventTypeId, eventId } = useParams();
   const { refetch: refetchCurrentBets } = useCurrentBets(eventId);
   const { refetch: refetchExposure } = useExposure(eventId);
@@ -36,6 +37,7 @@ const SpeedCashOut = ({ speedCashOut, setSpeedCashOut }) => {
       event_type_id: eventTypeId,
       market_name: speedCashOut?.market_name,
       event_name: speedCashOut?.event_name,
+      apk: closePopupForForever ? true : false,
     };
     mutate(payload, {
       onSuccess: (data) => {
