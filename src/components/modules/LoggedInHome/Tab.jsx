72d0364c62d56d578ae7e57dcad16dd4687734dd
sabbirHossain <sabbirshnt@gmail.com>
2026-07-06 14:03:56 +0600

    done

--- src/components/modules/LoggedInHome/Tab.jsx
+++ src/components/modules/LoggedInHome/Tab.jsx
@@ -15,11 +15,7 @@ const Tab = () => {
       name: "Inplay",
       imageSrc: "/images/menu-in_play.png",
     },
-    {
-      name: "Women's T20 WC",
-      imageSrc: "/images/menu-99998.png",
-      to: "/event-details/4/33439203",
-    },
+
     {
       name: "FIFA 2026",
       imageSrc: "/images/menu-99998.png",
